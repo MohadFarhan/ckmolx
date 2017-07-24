244e01bba710df88af39fbaee40c89e74cab58e2
MohaDFarhan <farhanbaksh@outlook.com>
2017-07-24 22:52:03 +0530

    SMS activity UI done

---
+++ app/src/main/java/com/example/mohammedfarhan/start/Activities/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.mohammedfarhan.start.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.mohammedfarhan.start.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button mobileSubmitBTN=(Button)findViewById(R.id.mobileSumbitBtn);
+        mobileSubmitBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LoginActivity.this,LocationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
